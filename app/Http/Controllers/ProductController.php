90b7c4c8518d3a913b80f4d672a70faa502cc8c6
francisgill42 <francisgill42@gmail.com>
2019-12-30 14:47:17 +0500

    enabled api middleware

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,7 +13,7 @@ class ProductController extends Controller
 {
      public function __construct()
     {
-        //$this->middleware('auth:api')->except('register','login','logout');
+        $this->middleware('auth:api')->except('register','login','logout');
     }
     public function index()
     {
